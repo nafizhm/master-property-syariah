7bd81bd5b294fe8fb8eafd65e7eb4799cca8a979
rojalingga <rojalingga2006@gmail.com>
2026-04-17 09:23:48 +0700

    fix : route marketing agent

--- resources/views/admin/marketing/marketing_agent/index.blade.php
+++ resources/views/admin/marketing/marketing_agent/index.blade.php
@@ -301,7 +301,7 @@
             submitBtn.prop('disabled', true);
 
             let id = $('#primary_id').val();
-            let url = id ? '{{ route('marketing-agent.update', ['marketing_agen' => ':id']) }}'.replace(
+            let url = id ? '{{ route('marketing-agent.update', ['marketing_agent' => ':id']) }}'.replace(
                     ':id',
                     id) :
                 '{{ route('marketing-agent.store') }}';
